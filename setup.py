78c2ca02f861eca62c708efae6a7a18a542d9554
Matthias Mittner <matthias.mittner@uit.no>
2020-06-27 12:12:40 +0200

    Enable PyPI
    - created PyPI package
    - added installation instructions
    
    See also: #5

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as fh:
+    requirements=fh.read().split()
+
 setuptools.setup(
     name="pypillometry",
-    version="1.0.1",
+    version="1.0.2",
     author="Matthias Mittner",
     author_email="matthias.mittner@uit.no",
     description="Pupillometry in Python.",
@@ -18,5 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=requirements,
     python_requires='>=3.6',
 )
