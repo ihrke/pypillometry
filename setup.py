9fe0c2641a1c1c1aa4f37220c1d6b0346dee7194
Matthias Mittner <matthias.mittner@uit.no>
2020-06-27 12:01:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pypillometry",
-    version="0.0.1",
+    version="1.0.1",
     author="Matthias Mittner",
     author_email="matthias.mittner@uit.no",
     description="Pupillometry in Python.",
