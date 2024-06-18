1f1ea72bfae396a32852953edbc1314fe866567b
Matthias Mittner <matthias.mittner@uit.no>
2024-06-18 15:54:22 +0200

    begin restructuring according to plan

--- pypillometry/__init__.py
+++ pypillometry/__init__.py
@@ -5,16 +5,11 @@ Pypillometry
 This is a python-package to help with processing of pupillometric data.
 """
 
-_inplace=False ## default for whether or not inplace-operations should be used
+__all__ = ["eyedata","plot","signal","convenience","io","erpd"]
 
-from .baseline import *
-from .convenience import *
-from .fakedata import *
-from .pupil import *
-from .eyedata_generic import *
-from .pupildata import *
-from .eyedata import *
-from .erpd import *
+from .eyedata.eyedatadict import EyeDataDict
+
+
 
 import os.path
 __package_path__ = os.path.abspath(os.path.dirname(__file__))
---
+++ pypillometry/eyedata/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["eyedatadict", "eyedata_generic","eyedata","fakedata","pupildata"]
