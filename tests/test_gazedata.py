d3dd106f5a0e8d4337b8ecf1e0aeef405770e398
Matthias Mittner <matthias.mittner@uit.no>
2024-06-19 16:28:42 +0200

    added scale() test in gazedata

---
+++ tests/test_gazedata.py
@@ -0,0 +1,37 @@
+import unittest
+import sys
+import numpy as np
+sys.path.insert(0,"..")
+import pypillometry as pp
+import pytest
+
+class TestGazeData(unittest.TestCase):
+    def setUp(self):
+        #self.d=pp.GazeData.from_file("data/test.pd")
+        pass
+    def test_from_file(self):
+        pass
+        #d=pp.GazeData.from_file("data/test.pd")
+        #self.assertEqual(d.__class__, pp.GazeData)
+    def test_scale(self):
+        d = pp.GazeData(sampling_rate=10, left_x=[1,2,1,2], left_y=[3,4,3,4])
+        d=d.scale(["x",'y'])
+        self.assertEqual(d.data["left","x"].mean(), 0)
+        self.assertEqual(d.data["left","y"].mean(), 0)
+        self.assertEqual(d.scale_params["left","x","mean"], 1.5)
+        self.assertEqual(d.scale_params["left","y","sd"], 0.5)
+    def test_scale_fixed(self):
+        d = pp.GazeData(sampling_rate=10, left_x=[1,2,1,2], left_y=[3,4,3,4])
+        scalepars = pp.Parameters({("mean","left","x"):1, 
+                                   ("mean","left","y"):3, 
+                                   ("sd","left","x"):0.5, 
+                                   ("sd","left","y"):0.5})
+        d=d.scale("x", mean=scalepars["mean"], sd=scalepars["sd"])
+        self.assertEqual(d.scale_params["left","x","mean"], 1)
+        self.assertEqual(d.scale_params["left","x","sd"], 0.5)
+    
+        #pytest.set_trace()
+        
+
+if __name__ == '__main__':
+    unittest.main()
